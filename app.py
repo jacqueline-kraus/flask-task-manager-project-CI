af78ae480c28ef7dcbc7245533af6498582cbd5b
Jacqueline Kraus <jacquelinekraus1992@gmail.com>
2021-07-27 16:09:47 +0200

    add delete category functionality

--- app.py
+++ app.py
@@ -174,10 +174,19 @@ def edit_category(category_id):
         }
         mongo.db.categories.update({"_id": ObjectId(category_id)} ,submit)
         flash("Category successfully updated")
-        return redirect(url_for("get_categories "))
+        return redirect(url_for("get_categories"))
     
     category = mongo.db.categories.find_one({"_id": ObjectId(category_id)})
     return render_template("edit_category.html", category=category)
+
+
+@app.route("/delete_category/<category_id>")
+def delete_category(category_id):
+    mongo.db.categories.remove({"_id": ObjectId(category_id)})
+    flash("Category successfully deleted")
+    return redirect(url_for("get_categories"))
+
+
 
 
 
